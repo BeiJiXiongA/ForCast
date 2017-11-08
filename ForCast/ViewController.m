d19692996aab2306dc84b3a259dbd8359b899aad
ZhangWei-SpaceHome <zhangwei36@kongjianjia.com>
2017-11-08 14:52:39 +0800

    Initial Commit

---
+++ ForCast/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ForCast
+//
+//  Created by ZhangWei-SpaceHome on 2017/11/8.
+//  Copyright © 2017年 zhangwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
