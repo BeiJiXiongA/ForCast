79bb255e3103f2c5a75527e192a0853b8f77c84d
ZhangWei-SpaceHome <zhangwei36@kongjianjia.com>
2017-11-08 16:14:18 +0800

    first commit

--- ForCast/HomeViewController.m
+++ ForCast/HomeViewController.m
@@ -1,29 +1,32 @@
+
 //
-//  ViewController.m
+//  HomeViewController.m
 //  ForCast
 //
 //  Created by ZhangWei-SpaceHome on 2017/11/8.
 //  Copyright © 2017年 zhangwei. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HomeViewController.h"
 
-@interface ViewController ()
+@interface HomeViewController ()
 
 @end
 
-@implementation ViewController
+@implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ForCast' do
+platform:ios, '8.0'
+pod 'AFNetworking'
+pod 'SQLitePersistentObjects'
+end
